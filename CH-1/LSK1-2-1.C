1494a2af5c50511efebf1ba9d96c611bb4b594f2
LakshSutariya <kajalsutariya996@gmail.com>
2023-05-18 14:43:42 +0530

    18/05/2023 | CH-1 introdtion to C language

---
+++ CH-1/LSK1-2-1.C
@@ -0,0 +1,15 @@
+#include<conio.h>
+#include<stdio.h>
+main()
+{
+	clrscr();
+	printf("hello world\n\n");
+	printf("name\t:laksh\n");
+	printf("sirname\t:sutariya\n");
+	printf("std\t:10\n");
+	printf("class\t:B\n");
+	printf("coursc\t:C language\n");
+	printf("hobby\t:codeing\n");
+	printf("father\t:sanjaybhai\n");
+	getch();
+}
