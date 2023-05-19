8e6c9f60956593d2ee3d2e6a72801852828f832e
LakshSutariya <133779973+LakshSutariya@users.noreply.github.com>
2023-05-19 19:16:20 +0530

    19/05/2023 | greenhorns

---
+++ GREEN.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{       int C;
+	float F;
+	clrscr();
+	printf("give any valu of C : ");
+	scanf("%d",&C);
+	F=(C*9/5)+32;
+	printf("F :%f",F);
+	getch();
+}
