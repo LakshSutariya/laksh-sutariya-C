7f6ebc9907cbe6bdde7b55d63b845ba550c3a89e
LakshSutariya <kajalsutariya996@gmail.com>
2023-06-12 06:26:39 -0700

    12/06/2023 | test-4

---
+++ test-4/LSK-T-3.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int j,i,s;
+	clrscr();
+	for(i=5;i>=1;i--)
+	{
+		for(s=1;s<=i-1;s++)
+		{
+			printf(" ");
+		}
+
+			for(j=i;j<=5;j++)
+			{
+				printf("%d",j);
+			}
+			printf("\n");
+	}
+	getch();
+
+}
---
+++ test-4/LSK-T-4.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int j,i,s,sp=5;
+	clrscr();
+	for(i=1;i<=5;i++)
+	{
+		for(s=5;s>=sp;s--)
+		{
+			printf(" ");
+		}
+		sp--;
+
+		for(j=5;j>=i;j--)
+		{
+			printf("%d",j%2);
+		}
+
+		printf("\n");
+	}
+	getch();
+
+}
