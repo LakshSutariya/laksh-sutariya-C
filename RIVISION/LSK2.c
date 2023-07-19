c8d8ae5db626eb3f556e0cbbb43f6728f4be5112
LakshSutariya <kajalsutariya996@gmail.com>
2023-07-19 06:00:17 -0700

    REVISION

---
+++ RIVISION/LSK2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main(){
+	int i,j,s;
+	for(i=1;i<=5;i++){
+		for(j=i;j>=1;j--){
+			printf("%d",j);
+		}
+		printf("\n");
+	}
+}
---
+++ RIVISION/LSK4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main(){
+	int i,j,s;
+	for(i=5;i>=1;i--){
+		for(j=5;j>=i;j--){
+			printf("%d",j);
+		}
+		printf("\n");
+	}
+}
